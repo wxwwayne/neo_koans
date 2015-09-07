815a3aa1513f63f4308396be597e1a5c150ac29a
wxwwayne <wxwwayne@gmail.com>
2015-09-07 14:24:56 -0400

    finish-keyword- argument

--- about_methods.rb
+++ about_methods.rb
@@ -7,18 +7,18 @@ end
 class AboutMethods < Neo::Koan
 
   def test_calling_global_methods
-    assert_equal __, my_global_method(2,3)
+    assert_equal 5, my_global_method(2,3)
   end
 
   def test_calling_global_methods_without_parentheses
     result = my_global_method 2, 3
-    assert_equal __, result
+    assert_equal 5, result
   end
 
   # (NOTE: We are Using eval below because the example code is
   # considered to be syntactically invalid).
   def test_sometimes_missing_parentheses_are_ambiguous
-    eval "assert_equal 5, my_global_method 2, 3" # ENABLE CHECK
+    eval "assert_equal 5, my_global_method(2, 3)" # ENABLE CHECK
     #
     # Ruby doesn't know if you mean:
     #
@@ -33,15 +33,15 @@ class AboutMethods < Neo::Koan
   # NOTE: wrong number of arguments is not a SYNTAX error, but a
   # runtime error.
   def test_calling_global_methods_with_wrong_number_of_arguments
-    exception = assert_raise(___) do
+    exception = assert_raise(ArgumentError) do
       my_global_method
     end
-    assert_match(/__/, exception.message)
+    assert_match(/arguments/, exception.message)
 
-    exception = assert_raise(___) do
+    exception = assert_raise(ArgumentError) do
       my_global_method(1,2,3)
     end
-    assert_match(/__/, exception.message)
+    assert_match(/arguments/, exception.message)
   end
 
   # ------------------------------------------------------------------
@@ -51,8 +51,8 @@ class AboutMethods < Neo::Koan
   end
 
   def test_calling_with_default_values
-    assert_equal [1, __], method_with_defaults(1)
-    assert_equal [1, __], method_with_defaults(1, 2)
+    assert_equal [1, :default_value], method_with_defaults(1)
+    assert_equal [1, 2], method_with_defaults(1, 2)
   end
 
   # ------------------------------------------------------------------
@@ -62,11 +62,16 @@ class AboutMethods < Neo::Koan
   end
 
   def test_calling_with_variable_arguments
-    assert_equal __, method_with_var_args.class
-    assert_equal __, method_with_var_args
-    assert_equal __, method_with_var_args(:one)
-    assert_equal __, method_with_var_args(:one, :two)
+    assert_equal Array, method_with_var_args.class
+    assert_equal [], method_with_var_args
+    assert_equal [:one], method_with_var_args(:one)
+    assert_equal [:one, :two], method_with_var_args(:one, :two)
   end
+  #So we put an asterisk in front of the variable name, and 
+  #that takes all of the inputs, and shoves them in an array. 
+  #So the class, will be Array. when nothing is passed into it, 
+  #it will return an empty array. As for the rest, it'l be an 
+  #array with each element being an individual argument.
 
   # ------------------------------------------------------------------
 
@@ -77,18 +82,19 @@ class AboutMethods < Neo::Koan
   end
 
   def test_method_with_explicit_return
-    assert_equal __, method_with_explicit_return
+    assert_equal :return_value, method_with_explicit_return
   end
-
   # ------------------------------------------------------------------
 
   def method_without_explicit_return
     :a_non_return_value
     :return_value
   end
+    #When we don't explicitly return, 
+    #we just get the very last thing we evaluated.
 
   def test_method_without_explicit_return
-    assert_equal __, method_without_explicit_return
+    assert_equal :return_value, method_without_explicit_return
   end
 
   # ------------------------------------------------------------------
@@ -98,13 +104,13 @@ class AboutMethods < Neo::Koan
   end
 
   def test_calling_methods_in_same_class
-    assert_equal __, my_method_in_the_same_class(3,4)
+    assert_equal 12, my_method_in_the_same_class(3,4)
   end
 
   def test_calling_methods_in_same_class_with_explicit_receiver
-    assert_equal __, self.my_method_in_the_same_class(3,4)
+    assert_equal 12, self.my_method_in_the_same_class(3,4)
   end
-
+    ### function in variable scope!!!
   # ------------------------------------------------------------------
 
   def my_private_method
@@ -113,15 +119,19 @@ class AboutMethods < Neo::Koan
   private :my_private_method
 
   def test_calling_private_methods_without_receiver
-    assert_equal __, my_private_method
+    assert_equal "a secret", my_private_method
   end
 
   def test_calling_private_methods_with_an_explicit_receiver
-    exception = assert_raise(___) do
+    exception = assert_raise(NoMethodError) do
       self.my_private_method
     end
-    assert_match /__/, exception.message
+    assert_match /private method/, exception.message
   end
+  ## we are defining a method called myprivatemethod, and 
+  ##making it private. We call it and it returns "a secret" 
+  ##as we expect. However, if we explicitly receive it, it 
+  ##can't see the method.
 
   # ------------------------------------------------------------------
 
@@ -139,12 +149,12 @@ class AboutMethods < Neo::Koan
 
   def test_calling_methods_in_other_objects_require_explicit_receiver
     rover = Dog.new
-    assert_equal __, rover.name
+    assert_equal "Fido", rover.name
   end
 
   def test_calling_private_methods_in_other_objects
     rover = Dog.new
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       rover.tail
     end
   end
--- about_regular_expressions.rb
+++ about_regular_expressions.rb
@@ -20,15 +20,22 @@ class AboutRegularExpressions < Neo::Koan
     assert_equal "ab", "abbcccddddeeeee"[/ab?/] # "ab" or "a" if no "ab"
     assert_equal "a", "abbcccddddeeeee"[/az?/]
   end
+  #You have a regex of /colou?r/. This would generate a hit on "color" as 
+  #well as "colour" as the question mark means that the u is optional.
 
   def test_plus_means_one_or_more
     assert_equal "bccc", "abbcccddddeeeee"[/bc+/] #"bccccc....." until no c
   end
+  # The plus will search for strings where the preceding character 
+  #appear one or more times. Example, /tre+/. That will hit "tree" 
+  #because you see it has two instances of e
 
   def test_asterisk_means_zero_or_more
     assert_equal "abb", "abbcccddddeeeee"[/ab*/]
     assert_equal "a", "abbcccddddeeeee"[/az*/]
     assert_equal "", "abbcccddddeeeee"[/z*/]
+    #The star will search for strings where the preceding
+    # character appears ZERO or more times.
 
     # THINK ABOUT IT:
     #
@@ -46,7 +53,8 @@ class AboutRegularExpressions < Neo::Koan
   def test_the_left_most_match_wins
     assert_equal "a", "abbccc az"[/az*/]
   end
-
+  ###OPTIONAL LIKE ?
+  ### two possible matches, left one will win!! 
   # ------------------------------------------------------------------
 
   def test_character_classes_give_options_for_a_character
@@ -66,6 +74,7 @@ class AboutRegularExpressions < Neo::Koan
   def test_slash_s_is_a_shortcut_for_a_whitespace_character_class
     assert_equal " \t\n", "space: \t\n"[/\s+/]
   end
+  ###what is after the space(\s)?
 
   def test_slash_w_is_a_shortcut_for_a_word_character_class
     # NOTE:  This is more like how a programmer might define a word.
@@ -76,6 +85,8 @@ class AboutRegularExpressions < Neo::Koan
   def test_period_is_a_shortcut_for_any_non_newline_character
     assert_equal "abc", "abc\n123"[/a.+/]
   end
+  ##the period(.) is a shortcut for any character(letter or number or symbol) 
+  #that is not a new line character, or \n.
 
   def test_a_character_class_can_be_negated
     assert_equal "the number is ", "the number is 42"[/[^0-9]+/]
@@ -92,53 +103,56 @@ class AboutRegularExpressions < Neo::Koan
   # ------------------------------------------------------------------
 
   def test_slash_a_anchors_to_the_start_of_the_string
-    assert_equal __, "start end"[/\Astart/]
-    assert_equal __, "start end"[/\Aend/]
+    assert_equal "start", "start end"[/\Astart/]
+    assert_equal nil, "start end"[/\Aend/]
   end
 
   def test_slash_z_anchors_to_the_end_of_the_string
-    assert_equal __, "start end"[/end\z/]
-    assert_equal __, "start end"[/start\z/]
+    assert_equal "end", "start end"[/end\z/]
+    assert_equal nil, "start end"[/start\z/]
   end
 
   def test_caret_anchors_to_the_start_of_lines
-    assert_equal __, "num 42\n2 lines"[/^\d+/]
+     assert_equal "2", "num42\n2 lines"[/^\d+/]
   end
 
   def test_dollar_sign_anchors_to_the_end_of_lines
-    assert_equal __, "2 lines\nnum 42"[/\d+$/]
+    assert_equal "42", "2 lines\nnum 42"[/\d+$/]
   end
-
+##The caret(like\A) is a strange one it anchors your search to 
+##the beginning of lines, and the dollar(like\z) sign is the opposite, 
+##it will only search the end of lines.
+##BUT \A AND \Z DO NOT CARE ABOUT "\n"!!!!!!! physically one line~~~~
   def test_slash_b_anchors_to_a_word_boundary
-    assert_equal __, "bovine vines"[/\bvine./]
+    assert_equal "vines", "bovine vines"[/\bvine./]
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_group_contents
-    assert_equal __, "ahahaha"[/(ha)+/]
+    assert_equal "hahaha", "ahahaha"[/(ha)+/]
   end
 
   # ------------------------------------------------------------------
 
   def test_parentheses_also_capture_matched_content_by_number
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 1]
-    assert_equal __, "Gray, James"[/(\w+), (\w+)/, 2]
+    assert_equal "Gray", "Gray, James"[/(\w+), (\w+)/, 1]
+    assert_equal "James", "Gray, James"[/(\w+), (\w+)/, 2]
   end
 
   def test_variables_can_also_be_used_to_access_captures
-    assert_equal __, "Name:  Gray, James"[/(\w+), (\w+)/]
-    assert_equal __, $1
-    assert_equal __, $2
+    assert_equal "Gray, James", "Name:  Gray, James"[/(\w+), (\w+)/]
+    assert_equal "Gray", $1
+    assert_equal "James", $2
   end
 
   # ------------------------------------------------------------------
 
   def test_a_vertical_pipe_means_or
     grays = /(James|Dana|Summer) Gray/
-    assert_equal __, "James Gray"[grays]
-    assert_equal __, "Summer Gray"[grays, 1]
-    assert_equal __, "Jim Gray"[grays, 1]
+    assert_equal "James Gray", "James Gray"[grays]
+    assert_equal "Summer", "Summer Gray"[grays, 1]
+    assert_equal nil, "Jim Gray"[grays, 1]
   end
 
   # THINK ABOUT IT:
@@ -148,14 +162,14 @@ class AboutRegularExpressions < Neo::Koan
   # ------------------------------------------------------------------
 
   def test_scan_is_like_find_all
-    assert_equal __, "one two-three".scan(/\w+/)
+    assert_equal ["one", "two", "three"], "one two-three".scan(/\w+/)
   end
 
   def test_sub_is_like_find_and_replace
-    assert_equal __, "one two-three".sub(/(t\w*)/) { $1[0, 1] }
+    assert_equal "one t-three", "one two-three".sub(/(t\w*)/) { $1[0, 1] }
   end
 
   def test_gsub_is_like_find_and_replace_all
-    assert_equal __, "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+    assert_equal "one t-t", "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
   end
 end
